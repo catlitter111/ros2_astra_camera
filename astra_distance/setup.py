03d7b38f0bc600c9c33278285ad0770687fade34
your name <your@email.com>
2025-10-19 17:07:35 +0800

    launch

--- astra_distance/setup.py
+++ astra_distance/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/astra_distance.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +25,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'astra_distance = astra_distance.astra_distance:main',
         ],
     },
 )
